fc29c1a769a9f93d2eaddb254aaeaa6085c2f4f1
Tronje Krop <tronje.krop@jactors.de>
2026-01-18 14:13:06 +0100

    feat: improve error support (#8)
    Signed-off-by: Tronje Krop <tronje.krop@jactors.de>

--- errors/errors.go
+++ errors/errors.go
@@ -3,6 +3,101 @@ package errors
 import (
 	"errors"
 	"fmt"
+
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+var (
+	// Is checks whether the given error matches a target error.
+	Is = errors.Is
+	// As casts the given error to a target type.
+	As = errors.As
+	// Join combines multiple errors into a single error.
+	Join = errors.Join
+)
+
+// Kubernetes error checkers.
+var (
+	// IsNotFound checks whether the given error is a Kubernetes not found
+	// error.
+	IsNotFound = kerrors.IsNotFound
+
+	// IsAlreadyExists checks whether the given error is a Kubernetes already
+	// exists error.
+	IsAlreadyExists = kerrors.IsAlreadyExists
+
+	// IsConflict checks whether the given error is a Kubernetes conflict
+	// error.
+	IsConflict = kerrors.IsConflict
+
+	// IsInvalid checks whether the given error is a Kubernetes invalid error.
+	IsInvalid = kerrors.IsInvalid
+
+	// IsGone checks whether the given error is a Kubernetes gone error.
+	IsGone = kerrors.IsGone
+
+	// IsResourceExpired checks whether the given error is a Kubernetes
+	// resource expired error.
+	IsResourceExpired = kerrors.IsResourceExpired
+
+	// IsNotAcceptable checks whether the given error is a Kubernetes not
+	// acceptable error.
+	IsNotAcceptable = kerrors.IsNotAcceptable
+
+	// IsUnsupportedMediaType checks whether the given error is a Kubernetes
+	// unsupported media type error.
+	IsUnsupportedMediaType = kerrors.IsUnsupportedMediaType
+
+	// IsMethodNotSupported checks whether the given error is a Kubernetes
+	// method not supported error.
+	IsMethodNotSupported = kerrors.IsMethodNotSupported
+
+	// IsServiceUnavailable checks whether the given error is a Kubernetes
+	// service unavailable error.
+	IsServiceUnavailable = kerrors.IsServiceUnavailable
+
+	// IsBadRequest checks whether the given error is a Kubernetes bad request
+	// error.
+	IsBadRequest = kerrors.IsBadRequest
+
+	// IsUnauthorized checks whether the given error is a Kubernetes
+	// unauthorized error.
+	IsUnauthorized = kerrors.IsUnauthorized
+
+	// IsForbidden checks whether the given error is a Kubernetes forbidden
+	// error.
+	IsForbidden = kerrors.IsForbidden
+
+	// IsTimeout checks whether the given error is a Kubernetes timeout error.
+	IsTimeout = kerrors.IsTimeout
+
+	// IsServerTimeout checks whether the given error is a Kubernetes server
+	// timeout error.
+	IsServerTimeout = kerrors.IsServerTimeout
+
+	// IsInternalError checks whether the given error is a Kubernetes internal
+	// error.
+	IsInternalError = kerrors.IsInternalError
+
+	// IsTooManyRequests checks whether the given error is a Kubernetes too
+	// many requests error.
+	IsTooManyRequests = kerrors.IsTooManyRequests
+
+	// IsRequestEntityTooLargeError checks whether the given error is a
+	// Kubernetes request entity too large error.
+	IsRequestEntityTooLargeError = kerrors.IsRequestEntityTooLargeError
+
+	// IsUnexpectedServerError checks whether the given error is a Kubernetes
+	// unexpected server error.
+	IsUnexpectedServerError = kerrors.IsUnexpectedServerError
+
+	// IsUnexpectedObjectError checks whether the given error is a Kubernetes
+	// unexpected object error.
+	IsUnexpectedObjectError = kerrors.IsUnexpectedObjectError
+
+	// IsStoreReadError checks whether the given error is a Kubernetes store
+	// read error.
+	IsStoreReadError = kerrors.IsStoreReadError
 )
 
 // Error is an abstract error type wrapping a given error.
@@ -10,14 +105,14 @@ type Error struct {
 	err error
 }
 
-// New creates a new error type with the given base error.
-func New(err error) *Error {
-	return &Error{err: err}
+// New creates a new wrapped error type with the given error message.
+func New(msg string) *Error {
+	return &Error{err: errors.New(msg)}
 }
 
-// NewError creates a new error type with the given error message.
-func NewError(msg string) *Error {
-	return &Error{err: errors.New(msg)}
+// Wrap creates a wrapped error type with the given base error.
+func Wrap(err error) *Error {
+	return &Error{err: err}
 }
 
 // Unwrap unwraps the underlying error.
